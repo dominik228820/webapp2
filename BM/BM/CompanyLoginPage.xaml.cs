6b2fa172e19f02cd33253b503b03e81c1b9970fe
dominik228820 <dominik228820@gmail.com>
2022-11-14 17:02:15 +0100

    webapp2

---
+++ BM/BM/CompanyLoginPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace BM
+{
+    public partial class CompanyLoginPage : ContentPage
+    {
+        public CompanyLoginPage()
+        {
+            InitializeComponent();
+
+            
+        }
+
+        private void Zaloguj_Button_Clicked(object sender, EventArgs e)
+        {
+            
+            if ((CompanyID.Text == "9876543210") && (CompanyPW.Text == "CompanyPW"))
+            {
+                Navigation.PushModalAsync(new WorkerLoginPage());
+            }
+            else
+            {
+                DisplayAlert("Uwaga", "Błędne dane!", "Close");
+            }
+        }
+
+
+        private void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+        {
+    
+        }
+    }
+}
---
+++ BM/BM/WorkerLoginPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace BM
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class WorkerLoginPage : ContentPage
+    {
+        public WorkerLoginPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Zaloguj_Clicked(object sender, EventArgs e)
+        {
+            if ((Login.Text == "Boss") && (Password.Text == "BossPW"))
+            {
+                Navigation.PushModalAsync(new Pulpit());
+            }
+            else
+            {
+                DisplayAlert("Uwaga", "Błędne dane!", "Close");
+            }
+        }
+    }
+}
