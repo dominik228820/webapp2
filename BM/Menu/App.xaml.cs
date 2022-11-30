3300e623f963bbce836720bba3daa026e94cb31c
dominik228820 <dominik228820@gmail.com>
2022-11-30 18:03:32 +0100

    Zarzadzanie klientami

---
+++ BM/Menu/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace BM
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new CompanyLoginPage()); //CompanyLoginPage
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
